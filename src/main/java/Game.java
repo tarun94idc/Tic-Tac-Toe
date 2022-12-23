f06bede338259c429db878be2eab06d8fd070bed
Tarun Manjunath <tarun.m1@idctechnologies.com>
2022-12-23 15:37:57 +0530

    added tic tac toe

---
+++ src/main/java/Game.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Game extends JFrame {
+
+    public Game() {
+        this.add(new Graphics());
+        this.setTitle("Tic Tac Toe");
+        this.pack();
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
